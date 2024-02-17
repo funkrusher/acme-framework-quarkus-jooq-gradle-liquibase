90dc49ca895a9d3f41f8c4413e3547f407f13547
Bernd Huber <45738360+funkrusher@users.noreply.github.com>
2024-02-17 21:29:28 +0100

    start trying to make the extractRecords process easier for developer (1)
    start trying to make the extractRecords process easier for developer (1)

--- acme-jooq-backend/src/main/java/org/acme/daos/view/ProductViewDAO.java
+++ acme-jooq-backend/src/main/java/org/acme/daos/view/ProductViewDAO.java
@@ -3,6 +3,7 @@ package org.acme.daos.view;
 import org.acme.daos.AbstractViewDAO;
 import org.acme.dtos.ProductDTO;
 import org.acme.dtos.ProductLangDTO;
+import org.acme.generated.AbstractDTO;
 import org.acme.generated.jooq_testshop.tables.Product;
 import org.acme.generated.jooq_testshop.tables.ProductLang;
 import org.acme.generated.jooq_testshop.tables.records.ProductLangRecord;
@@ -15,6 +16,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * ProductViewDAO
@@ -46,29 +51,39 @@ public class ProductViewDAO extends AbstractViewDAO<ProductRecord, ProductDTO, L
                         .eq(Product.PRODUCT.PRODUCTID));
     }
 
+
+
+    private List<Record> extractRecords(List<Record> records, List<Field<?>> uniqueFields) {
+        Map<String, Record> uniqueRecords = new HashMap<>();
+        for (Record record : records) {
+            String key = "";
+            for (Field<?> uniqueField : uniqueFields) {
+                String test = record.getValue(uniqueField).toString();
+                key += test;
+            }
+            uniqueRecords.put(key, record);
+        }
+        return uniqueRecords.values().stream().toList();
+    }
+
+
+
     @Override
     protected List<ProductDTO> recordsToView(List<Record> records) {
-        List<ProductDTO> products = new ArrayList<>();
-        Map<Long, ProductDTO> productMap = new HashMap<>();
+        // uniquely get entries and swallow duplicates.
+        List<ProductDTO> products = extractRecords(records, List.of(Product.PRODUCT.PRODUCTID))
+                .stream().map(r -> r.into(new ProductRecord()).into(new ProductDTO())).toList();
+         Map<Long, List<ProductLangDTO>> langs = extractRecords(records, List.of(ProductLang.PRODUCT_LANG.PRODUCTID, ProductLang.PRODUCT_LANG.LANGID))
+                .stream().map(r -> r.into(new ProductLangRecord()).into(new ProductLangDTO()))
+                .collect(groupingBy(ProductLangDTO::getProductId));
 
-        for (Record record : records) {
-            Long productId = record.get(Product.PRODUCT.PRODUCTID);
-            ProductDTO productDTO = productMap.get(productId);
-
-            if (productDTO == null) {
-                ProductRecord productRecord = record.into(new ProductRecord());
-                productDTO = productRecord.into(new ProductDTO());
-                List<ProductLangDTO> xLangs = new ArrayList<>();
-                productDTO.setLangs(xLangs);
-                products.add(productDTO);
-                productMap.put(productId, productDTO);
-            }
-            ProductLangRecord xLangRecord = record.into(new ProductLangRecord());
-            ProductLangDTO xLang = xLangRecord.into(new ProductLangDTO());
-            productDTO.getLangs().add(xLang);
-
-            if (xLang.getLangId().equals(requestContext().getLangId())) {
-                productDTO.setLang(xLang);
+        for (ProductDTO product : products) {
+            List<ProductLangDTO> productLangs = langs.get(product.getProductId());
+            product.setLangs(productLangs);
+            for (ProductLangDTO productLang : productLangs) {
+                if (productLang.getLangId().equals(requestContext().getLangId())) {
+                    product.setLang(productLang);
+                }
             }
         }
         return products;
